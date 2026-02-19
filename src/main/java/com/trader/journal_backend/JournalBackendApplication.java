6cb99fb3eb0c0f3da3044a6590d1d208803429ba
Junia Tran <tvandinh93@gmail.com>
2026-02-19 12:29:14 +0700

    chore: init backend

---
+++ src/main/java/com/trader/journal_backend/JournalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trader.journal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trader/journal_backend/JournalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trader.journal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
